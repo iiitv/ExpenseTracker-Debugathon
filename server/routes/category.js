5952f27cb5dd3f89f0a6f42e09db308506a883a0
Devyash Saini <dysaini2004@gmail.com>
2024-11-17 17:35:23 +0530

    Fixed category delete

--- server/routes/category.js
+++ server/routes/category.js
@@ -3,8 +3,8 @@ const router = Router()
 import User from '../models/User.js'
 
 router.delete('/:name',async (req,res)=>{
-  req.user.categories.filter(n=>n!==req.params.name)
-  const user = await User.findByIdAndUpdate(req.user.id,{categories:req.user.categories},{new:true})
+  const newCategories=  req.user.categories.filter(n=>n!==req.params.name)
+  const user = await User.findByIdAndUpdate(req.user.id,{categories:newCategories},{new:true})
   res.status(200).json(user)
 })
 
