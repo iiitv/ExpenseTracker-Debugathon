577d27324a55f330a774ad07c7736e5b771e82d6
Devyash Saini <dysaini2004@gmail.com>
2024-11-17 17:43:07 +0530

    Fixed edit transactions

--- server/routes/category.js
+++ server/routes/category.js
@@ -3,7 +3,7 @@ const router = Router()
 import User from '../models/User.js'
 
 router.delete('/:name',async (req,res)=>{
-  const newCategories=  req.user.categories.filter(n=>n!==req.params.name)
+  const newCategories =  req.user.categories.filter(n=>n!==req.params.name)
   const user = await User.findByIdAndUpdate(req.user.id,{categories:newCategories},{new:true})
   res.status(200).json(user)
 })
